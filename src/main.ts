8607d5a86a24ecd07255f5e7c7613f37d70e56f8
devkai <irakoseimei4@gmail.com>
2023-12-06 11:30:20 +0900

    Any Type

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,10 @@ a = undefined;
 const b: number[] = [1, 2, 3];
 b.push(4);
 
+// Any Type(never use this)
+let c:any = 'whatever';
+c = 3;
+c = 'string';
+const d = fetch('anything').then(res => res.json()).then(data => {
+  data // data: any
+})
